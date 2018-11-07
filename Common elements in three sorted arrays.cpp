10aebda8fc0f943f936a3b7c07862794e8da7b4c
Rohan Gupta <rg.rohan26@gmail.com>
2018-11-08 00:29:37 +0530

    Create Common elements in three sorted arrays.cpp

---
+++ Common elements in three sorted arrays.cpp
@@ -0,0 +1,24 @@
+/* Given three increasingly sorted arrays A, B, C of sizes N1, N2, and N3 respectively, you need to print all common elements in these arrays.
+
+Input:
+First line contains a single integer T denoting the total number of test cases. T testcases follow. Each testcase contains four lines of input. First line consists of 3 integers N1, N2 and N3, denoting the sizes of arrays A, B, C respectively. The second line contains N1 elements of A array. The third lines contains N2 elements of B array. The fourth lines contains N3 elements of C array.
+Output:
+For each testcase, print the common elements of array. If not possible then print -1.
+
+Constraints:
+1 <= T <= 100
+1 <= N1, N2, N3 <= 107
+1 <= Ai, Bi, Ci <= 1018
+
+Sample Input:
+1
+6 5 8
+1 5 10 20 40 80
+6 7 20 80 100
+3 4 15 20 30 70 80 120
+Sample Output:
+20 80
+*/
+
+//Program in C++ -
+
